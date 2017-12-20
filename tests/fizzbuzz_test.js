f76ae8d1c236fba479efcb311083f35d77f6b80d
Adam Fortuna <adam@fortuna.name>
2017-12-20 13:43:41 -0700

    Added fizzbuzz tests

---
+++ tests/fizzbuzz_test.js
@@ -0,0 +1,101 @@
+// Goal: In `app/fizzbuzz.js`, export a function that takes 1 argument - a print function. Your exported function should call this function for all numbers from 1 to 100. But for multiples of three call it with “Fizz” instead of the number and for the multiples of five call it with “Buzz”. For numbers which are multiples of both three and five call it with “FizzBuzz”.
+// Requirements: Make all of these tests pass.
+// To run: node_modules/.bin/mocha -w tests/fizzbuzz_test.js
+
+var assert = require('chai').assert,
+    sinon = require('sinon'),
+    _ = require('lodash'),
+    fizzbuzz;
+
+describe('fizzbuzz', function() {
+  before(function() {
+    try {
+      fizzbuzz = require('../app/fizzbuzz.js');
+    } catch(e) {}
+  });
+
+  it('Make sure to export a function using `module.exports`. @fizzbuzz', function() {
+    assert.equal(typeof(fizzbuzz), "function", 'Make sure to export a function using `module.exports`.');
+  });
+
+  it('Your function should accept 1 argument - the `print` function. @fizzbuzz', function() {
+    assert.equal(fizzbuzz.length, 1, 'Your function should accept 1 argument.');
+  });
+
+  describe('execution', function() {
+    it("Your function didn't finish running. Is there an infinite loop? @fizzbuzz", function() {
+      try {
+        fizzbuzz(function() {} );
+      } catch(e) {
+        assert(false, "We tried calling your function and it came back with an error: `"+e.message+"`.");
+      }
+    });
+  });
+
+  describe('results', function() {
+    var spy;
+
+    beforeEach(function() {
+      spy = sinon.spy();
+      try {
+        fizzbuzz(spy);
+      } catch(e) {}
+    });
+
+    afterEach(function() {
+    });
+
+    it("calls `console.log` @fizzbuzz", function() {
+      assert(spy.called, "Make sure to call the passed in `print` in your function to print the results.");
+    });
+
+    describe('first 5', function() {
+      it("starts off right @fizzbuzz", function() {
+        var first = spy.firstCall.args[0],
+            second = spy.secondCall.args[0];
+        assert.equal(+first, 1, "Make sure the 1st call to `console.log` is `1`. The 1st call from your `fizzBuzz` function was with the argument: `"+first+"`.");
+        assert.equal(+second, 2, "Make sure the 2nd call to `console.log` is `2`. The 2nd call from your `fizzBuzz` function was with the argument: `"+second+"`.");
+      });
+      it("uses fizz @fizzbuzz", function() {
+        var third = spy.getCall(2).args[0];
+        assert.equal(third, "Fizz", "The 3rd call to `print` should pass in `Fizz`, but your 3rd call passed the argument: `"+third+"`.");
+      });
+      it("goes back to numbers @fizzbuzz", function() {
+        var fourth = spy.getCall(3).args[0];
+        assert.equal(fourth, 4, "The 4th call to `print` should pass in `4`, but your 4th call passed the argument: `"+fourth+"`.");
+      });
+      it("uses buzz @fizzbuzz", function() {
+        var fifth = spy.getCall(4).args[0];
+        assert.equal(fifth, "Buzz", "The 5th call to `print` should pass in `Buzz`, but your 4th call passed the argument: `"+fifth+"`.");
+      });
+    });
+
+    describe('overall', function() {
+      it('calls `print` 100 times @fizzbuzz', function() {
+        assert.equal(spy.callCount, 100, "Make sure to call `print` exactly 100 times. Your function calls it `"+spy.callCount+"` times.");
+      });
+
+      it('prints the correct thing for each iteraction @fizzbuzz', function() {
+        var spyCall, spyCallArg, expected;
+        for(var i=1; i<=100; i++) {
+          spyCall = spy.getCall(i-1);
+          assert(spyCall, "We ran into an error getting call number "+i+". Make sure you are calling `print` 100 times.");
+
+          spyCallArg = spyCall.args[0];
+
+          if((i%3 == 0) && (i%5 == 0)) {
+            expected = "FizzBuzz";
+          } else if(i%5 == 0) {
+            expected = "Buzz";
+          } else if(i%3 == 0) {
+            expected = "Fizz"
+          } else {
+            expected = i;
+          }
+
+          assert.equal(spyCallArg, expected, "Looks like for call number `"+i+"`, we expected your code to call `print` with `"+expected+"`, but it called `print` with `"+spyCallArg+"`.");
+        }
+      });
+    });
+  });
+});
--- tests/reverse_test.js
+++ tests/reverse_test.js
@@ -1,6 +1,6 @@
 // Goal: Write a function that will reverse an array.
 // Requirements: Make all of these tests pass without using the JavaScript reverse method.
-// To run: mocha -w tests/1_reverse_test.js
+// To run: node_modules/.bin/mocha -w tests/reverse_test.js
 
 var assert = require('chai').assert,
     sinon = require('sinon'),
@@ -15,11 +15,11 @@ describe('reverse', function() {
   });
 
   it('Make sure to export a function using `module.exports`. @reverse', function() {
-    assert.equal(typeof(reverse), "function");
+    assert.equal(typeof(reverse), "function", 'Make sure to export a function using `module.exports`.');
   });
 
   it('Your function should accept 1 argument - the array we want to reverse. @reverse', function() {
-    assert.equal(reverse.length, 1);
+    assert.equal(reverse.length, 1, 'Your function should accept 1 argument - the array we want to reverse.');
   });
 
   it("It looks like you used the native JavaScript `reverse` method. Try to solve this problem without using this one. @reverse", function() {
@@ -29,14 +29,14 @@ describe('reverse', function() {
 
     reverse(input);
 
-    assert.equal(spy.callCount, 0)
+    assert.equal(spy.callCount, 0, "It looks like you used the native JavaScript `reverse` method. Try to solve this problem without using this one.")
 
   });
 
   describe('with an empty array', function() {
     it('When passed an empty array, your function should return an empty array. @reverse', function() {
       var result = reverse([]);
-      assert.deepEqual(result, []);
+      assert.deepEqual(result, [], 'When passed an empty array, your function should return an empty array.');
     });
   });
 
@@ -45,7 +45,7 @@ describe('reverse', function() {
     it('When passed an array with one item, your function should return the same array. @reverse', function() {
       var key = Math.random();
       var result = reverse([key]);
-      assert.deepEqual(result, [key]);
+      assert.deepEqual(result, [key], 'When passed an array with one item, your function should return the same array.');
     });
   });
 
@@ -56,18 +56,18 @@ describe('reverse', function() {
     it('Your function is modifying the passed in argument. This means that the caller may see unforseen issues using your function. Make sure not to modify the passed in array. @reverse', function() {
       var clonedArray = _.clone(longArray);
       reverse(longArray)
-      assert.deepEqual(longArray, clonedArray);
+      assert.deepEqual(longArray, clonedArray, 'Your function is modifying the passed in argument. This means that the caller may see unforseen issues using your function. Make sure not to modify the passed in array.');
     });
 
     it('When called, the array should have the same length as passed in array. @reverse', function() {
       var reverseResultLength = reverse(longArray).length
-      assert.equal(reverseResultLength, longArray.length, "We tried calling your function with an array with `"+longArray.length+"` elements but your reverse function returned an array with `"+reverseResultLength+"` item(s). @reverse");
+      assert.equal(reverseResultLength, longArray.length, "We tried calling your function with an array with `"+longArray.length+"` elements but your reverse function returned an array with `"+reverseResultLength+"` item(s).");
     });
 
     it('The `reverse` function should return the reversed array. @reverse', function() {
       var reverseResult = reverse(longArray),
           correct = [5,4,3,2,1];
-      assert.deepEqual(reverseResult, correct, "We called your reverse function passing in `"+JSON.stringify(longArray)+" and expected to get back `" + JSON.stringify(correct) + "`, but it returned `"+JSON.stringify(reverseResult)+"`. @reverse");
+      assert.deepEqual(reverseResult, correct, "We called your reverse function passing in `"+JSON.stringify(longArray)+"` and expected to get back `" + JSON.stringify(correct) + "`, but it returned `"+JSON.stringify(reverseResult)+"`.");
     });
   });
 });
